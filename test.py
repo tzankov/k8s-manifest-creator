4ffc8661067d6ff244eabe2905895ac94f8d63ca
TT <tzankotz@hotmail.com>
2021-01-03 21:56:39 +0000

    fixed test.py issues

--- test.py
+++ test.py
@@ -2,19 +2,21 @@ import pytest
 import filecmp
 import os
 import sys
+import shutil
+from time import sleep
 from creator.creator import Creator
 
 
-class TestClass:
-    def compare_files(self):
-        c = Creator('default_k8s_parameters.yaml')
-        c.load_parameter_file()
-        c.validate_objects()
-        c.k8s_create_manifest()
+def test_compare_files():
+    c = Creator('default_k8s_parameters.yaml')
+    c.load_parameter_file()
+    c.validate_objects()
+    c.k8s_create_manifest()
 
-        cwd = os.getcwd()
+    sleep(3)
 
-        new_service_config = cwd + '/tzankoswebpage/service.yaml'
-        test_service_config = cwd + 'test_service_config.yaml'
-        assert filecmp.cmp(new_service_config, test_service_config, shallow=False)
+    cwd = os.getcwd()
 
+    new_service_config = cwd + '/tzankoswebpage/k8s/service.yaml'
+    test_service_config = cwd + '/test_service_config.yaml'
+    assert filecmp.cmp(new_service_config, test_service_config, shallow=False)
